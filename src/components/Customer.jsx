bb24ba787a6c49db32a8908bc01bbe4835d1f6aa
Dario Sturiale <dariosturiale@Darios-MacBook-Pro.local>
2023-12-03 23:20:23 +0200

    Osa2

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -4,8 +4,20 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 import '../App.css';
 
+import { Button } from '@mui/material';
+
 const Customer = () => {
   const [customers, setCustomers] = useState([]);
+  const [newCustomer, setNewCustomer] = useState({
+    firstname: '',
+    lastname: '',
+    streetaddress: '',
+    postcode: '',
+    city: '',
+    email: '',
+    phone: '',
+  });
+  const [editingCustomer, setEditingCustomer] = useState(null);
 
   useEffect(() => {
     fetchCustomers();
@@ -18,14 +30,106 @@ const Customer = () => {
       .catch(error => console.error(error));
   };
 
+  const handleAddCustomer = () => {
+    fetch('http://traineeapp.azurewebsites.net/api/customers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newCustomer),
+    })
+      .then(response => response.json())
+      .then(() => {
+        fetchCustomers();
+        setNewCustomer({
+          firstname: '',
+          lastname: '',
+          streetaddress: '',
+          postcode: '',
+          city: '',
+          email: '',
+          phone: '',
+        });
+      })
+      .catch(error => console.error(error));
+  };
+
+  const deleteCustomer = (params) => {
+    const customerLink = params.data.links.find(link => link.rel === 'self').href;
+    const customerId = customerLink.split('/').pop();
+
+    fetch(`http://traineeapp.azurewebsites.net/api/customers/${customerId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchCustomers();
+        } else {
+          console.error(`Failed to delete customer with ID ${customerId}`);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const editCustomer = (params) => {
+    const customerLink = params.data.links.find(link => link.rel === 'self').href;
+    const customerId = customerLink.split('/').pop();
+
+    fetch(`http://traineeapp.azurewebsites.net/api/customers/${customerId}`)
+      .then(response => response.json())
+      .then(data => {
+        setEditingCustomer(data);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleUpdateCustomer = () => {
+    if (!editingCustomer) {
+      console.error("No customer data for update");
+      return;
+    }
+  
+    const customerLink = editingCustomer.links.find((link) => link.rel === 'self').href;
+    const customerId = customerLink.split('/').pop();
+  
+    fetch(`http://traineeapp.azurewebsites.net/api/customers/${customerId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editingCustomer),
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchCustomers();
+          setEditingCustomer(null);
+        } else {
+          console.error(`Failed to update customer with ID ${customerId}`);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
   const columns = [
     { headerName: 'First Name', field: 'firstname', sortable: true, filter: true },
     { headerName: 'Last Name', field: 'lastname', sortable: true, filter: true },
     { headerName: 'Street Address', field: 'streetaddress', sortable: true, filter: true },
     { headerName: 'Postcode', field: 'postcode', sortable: true, filter: true },
     { headerName: 'City', field: 'city', sortable: true, filter: true },
-    { headerName: 'Email', field: 'email', sortable: true, filter: true  },
+    { headerName: 'Email', field: 'email', sortable: true, filter: true },
     { headerName: 'Phone', field: 'phone', sortable: true, filter: true },
+    {
+      cellRenderer: params =>
+        <Button size="small" onClick={() => deleteCustomer(params)}>
+          Delete
+        </Button>,
+    },
+    {
+      cellRenderer: params =>
+        <Button size="small" onClick={() => editCustomer(params)}>
+          Edit
+        </Button>,
+    },
   ];
 
   return (
@@ -33,11 +137,116 @@ const Customer = () => {
       <div style={{ paddingLeft: '18vw' }}>
         <h2>Customer List</h2>
       </div>
+      
       <div
         className="ag-theme-material"
-        style={{ height: '700px', width: '70%', margin: 'auto' }}
+        style={{  width: '70%', margin: 'auto' }}
       >
-        <AgGridReact columnDefs={columns} rowData={customers} />
+        <AgGridReact columnDefs={columns} rowData={customers} domLayout='autoHeight'/>
+      </div>
+      <div className='addNew'> 
+        <h2>{editingCustomer ? 'Edit Customer' : 'Add New Customer'}</h2>
+        <div>
+          <label>First Name:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.firstname : newCustomer.firstname}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, firstname: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, firstname: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.lastname : newCustomer.lastname}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, lastname: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, lastname: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>Street Address:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.streetaddress : newCustomer.streetaddress}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, streetaddress: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, streetaddress: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>Postcode:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.postcode : newCustomer.postcode}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, postcode: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, postcode: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>City:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.city : newCustomer.city}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, city: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, city: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.email : newCustomer.email}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, email: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, email: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <div>
+          <label>Phone:</label>
+          <input
+            type="text"
+            value={editingCustomer ? editingCustomer.phone : newCustomer.phone}
+            onChange={(e) => {
+              if (editingCustomer) {
+                setEditingCustomer({ ...editingCustomer, phone: e.target.value });
+              } else {
+                setNewCustomer({ ...newCustomer, phone: e.target.value });
+              }
+            }}
+          />
+        </div>
+        <button onClick={editingCustomer ? handleUpdateCustomer : handleAddCustomer}>
+          {editingCustomer ? 'Update Customer' : 'Add Customer'}
+        </button>
       </div>
     </div>
   );
--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -1,31 +1,46 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 import moment from 'moment';
 import 'moment/locale/fi';
 import '../App.css';
-
-const columns = [
-  {flex: "1", field: "date", sortable: true, filter: true, valueFormatter: params => moment(params.value).format('DD.MM.YYYY HH:mm') },
-  {flex: "1",  field: "duration", sortable: true, filter: true},
-  {flex: "1",  field: "activity", sortable: true, filter: true},
-  {flex: "1",  field: "customer", sortable: true, filter: true,  valueGetter: params => {
-    const customer = params.data.customer;
-    return `${customer.firstname} ${customer.lastname}`;
-    }}
- 
-  
-];
+import { Button } from '@mui/material';
 
 function Training() {
-
   const [training, setTraining] = useState([]);
+  const [customers, setCustomers] = useState([]);
+  const [newTraining, setNewTraining] = useState({
+    date: '',
+    activity: '',
+    duration: '',
+    customer: '',
+  });
 
-
+  const columns = [
+    { flex: '4', field: 'date', sortable: true, filter: true, valueFormatter: params => moment(params.value).format('DD.MM.YYYY HH:mm') },
+    { flex: '4', field: 'duration', sortable: true, filter: true },
+    { flex: '3', field: 'activity', sortable: true, filter: true },
+    {
+      flex: '3',
+      field: 'customer',
+      sortable: true,
+      filter: true,
+      valueGetter: params => {
+        const customer = params.data.customer;
+        return `${customer.firstname} ${customer.lastname}`;
+      },
+    },
+    {
+      flex: '2',
+      cellRenderer: params => <Button size="small" onClick={() => deleteTraining(params)}>Delete</Button>,
+    },
+    
+  ];
 
   useEffect(() => {
     fetchData();
+    fetchCustomers();
   }, []);
 
   const fetchData = () => {
@@ -33,27 +48,116 @@ function Training() {
       .then(response => response.json())
       .then(data => setTraining(data))
       .catch(error => console.error(error));
-  }
+  };
 
-  
+  const fetchCustomers = () => {
+    fetch('https://traineeapp.azurewebsites.net/api/customers')
+      .then(response => response.json())
+      .then(data => setCustomers(data.content))
+      .catch(error => console.error(error));
+  };
+
+  const handleAddTraining = () => {
+    const selectedCustomer = customers.find(c => `${c.firstname} ${c.lastname}` === newTraining.customer);
+
+    if (selectedCustomer) {
+      newTraining.customer = selectedCustomer.links.find(link => link.rel === 'self').href;
+    }
+
+    fetch('http://traineeapp.azurewebsites.net/api/trainings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTraining),
+    })
+      .then(response => response.json())
+      .then(() => {
+        fetchData();
+        setNewTraining({
+          date: '',
+          activity: '',
+          duration: '',
+          customer: '',
+        });
+      })
+      .catch(error => console.error(error));
+  };
+
+  const deleteTraining = (params) => {
+    const trainingId = params.data.id;
+
+    console.log("Delete Commenced:", trainingId);
+
+    fetch(`http://traineeapp.azurewebsites.net/api/trainings/${trainingId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchData();
+        } else {
+          console.error('Failed to delete training ${trainingId}.');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div>
-       <div style={{ paddingLeft: '18vw' }}>
-        <h2>Trainings Page</h2> 
+      <div style={{ paddingLeft: '18vw' }}>
+        <h2>Trainings Page</h2>
       </div>
-    <div className="ag-theme-material"
-      style={{ height: '700px', width: '70%', margin: 'auto' }}>
-        
-      <AgGridReact
-        columnDefs={columns}
-        rowData={training}>
-        
-      </AgGridReact>
+      
+      <div
+        className="ag-theme-material"
+        style={{ width: '70%', margin: 'auto' }}
+      >
+        <AgGridReact columnDefs={columns} rowData={training} domLayout='autoHeight' />
+      </div>
+      <div className="addNew">
+        <h2>Add New Training</h2>
+        <div>
+          <label>Date:</label>
+          <input
+            type="datetime-local"
+            value={newTraining.date}
+            onChange={(e) => setNewTraining({ ...newTraining, date: e.target.value })}
+          />
+        </div>
+        <div>
+          <label>Activity:</label>
+          <input
+            type="text"
+            value={newTraining.activity}
+            onChange={(e) => setNewTraining({ ...newTraining, activity: e.target.value })}
+          />
+        </div>
+        <div>
+          <label>Duration:</label>
+          <input
+            type="text"
+            value={newTraining.duration}
+            onChange={(e) => setNewTraining({ ...newTraining, duration: e.target.value })}
+          />
+        </div>
+        <div>
+          <label>Customer:</label>
+          <select
+            value={newTraining.customer}
+            onChange={(e) => setNewTraining({ ...newTraining, customer: e.target.value })}
+          >
+            <option value="" disabled>Select Customer</option>
+            {customers.map(c => (
+              <option key={c.links.find(link => link.rel === 'self').href}>
+                {`${c.firstname} ${c.lastname}`}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button onClick={handleAddTraining}>Add Training</button>
+      </div>
     </div>
-   
-
-    </div>
-  )
+  );
 }
+
 export default Training;
